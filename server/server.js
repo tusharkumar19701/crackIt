4c666eb383035ac6ba800201b0296cab5690c2e6
Tushar Kumar <tusharkumar19701@gmail.com>
2026-02-04 20:10:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ const app = express();
 
 //middleware fro cors
 app.use(cors({
-    origin: "*",
-    methods: ["GET","POST","PUT","DELETE"],
-    allowedHeaders: ["Content-Type","Authorization"]
+  origin: "https://crack-it-qtvl.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 connectDb();
